117c3dbd088a4fef670f6b76eb25c90e745eb53d
Fabian Köhler <me@fkoehler.org>
2024-03-22 16:27:30 +0800

    Tweak nix dependencies for pygame.

--- overrides/default.nix
+++ overrides/default.nix
@@ -2224,14 +2224,15 @@ lib.composeManyExtensions [
         _old: rec {
           nativeBuildInputs = [
             pkg-config
-            pkgs.SDL
+            pkgs.SDL2
           ];
 
           buildInputs = [
-            pkgs.SDL
-            pkgs.SDL_image
-            pkgs.SDL_mixer
-            pkgs.SDL_ttf
+            pkgs.SDL2
+            pkgs.SDL2_image
+            pkgs.SDL2_mixer
+            pkgs.SDL2_ttf
+            pkgs.portmidi
             pkgs.libpng
             pkgs.libjpeg
             pkgs.portmidi
